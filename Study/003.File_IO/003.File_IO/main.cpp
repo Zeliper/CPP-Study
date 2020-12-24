99c424aab31389788b2c97d5c2af0ab0e6bc0da6
itiniti <mack4ford@gmail.com>
2020-12-25 02:54:27 +0900

    i really hate this

--- Study/003.File_IO/003.File_IO/main.cpp
+++ Study/003.File_IO/003.File_IO/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+ofstream fout;
+fstream fin, fot;;
 
 void clr();
 void printMenu();
@@ -14,24 +17,57 @@ MEMO:
 */
 
 //화면 지우는 함수
-void clr(){ cout << u8"\033[2J\033[1;1H"; }
+void clr()
+{ 
+	system("cls");
+}
 
 //메뉴를 화면에 출력하는 함수 (입력도 여기서 받는다)
 void printMenu() {
-
+	cout << " Type 1 or 2 (1 = Write data / 2 = Load data) " << endl;
+	int input;
+	cin >> input;
+	if (input == 1) {
+		wfile();
+	}
+	else if (input == 2) {
+		rfile();		
+	}	
 }
 
 //텍스트 파일에 문자열을 쓰는 함수
 void wfile() {
+	clr();
+	fin.open("data.txt",ios::app);
+	cout << " Type any data to save. " << endl;
 
+	string input;
+	cin>>input;
+	fin << input;	
+	fin.close();
+
+	cout << " Data save complete " << endl << endl;
 }
 
 //텍스트 파일의 문자열을 읽는 함수
 void rfile() {
-
+	clr();
+	string line;
+	fot.open("data.txt");
+	if (fot.is_open()) {
+		while (getline(fot,line))
+		{
+			cout << line << endl;
+		}
+	}
+	fot.close();
+	cout << " Loading Complete ! " << endl << endl;
+	
 }
 
 //반복문을 사용하여 프로그램이 하나의 동작이후 꺼지지 않도록 한다.
-void main() {
-
+int main(void) {	
+	while (1) {
+		printMenu();
+	}
 }

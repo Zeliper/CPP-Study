1ed9611f924df0a50d092ad007d97eb14530ca94
Zeliper <dhtmddn13213@naver.com>
2020-12-24 10:17:23 +0900

    Create Third Lacture

---
+++ 003.File_IO/003.File_IO/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void clr();
+void printMenu();
+void wfile();
+void rfile();
+
+/*
+MEMO:
+	동작:	프로그램 시작시 메뉴가 화면에 출력되고 1을 입력하면 data.txt파일에 사용자가 자료를 입력할 수 있고, 2를 입력하면 data.txt파일의 내용을 읽어 콘솔에 출력한다.
+	ps.		화면을 지우려면 clr() 함수를 호출하면 된다.
+*/
+
+//화면 지우는 함수
+void clr(){ cout << u8"\033[2J\033[1;1H"; }
+
+//메뉴를 화면에 출력하는 함수 (입력도 여기서 받는다)
+void printMenu() {
+
+}
+
+//텍스트 파일에 문자열을 쓰는 함수
+void wfile() {
+
+}
+
+//텍스트 파일의 문자열을 읽는 함수
+void rfile() {
+
+}
+
+//반복문을 사용하여 프로그램이 하나의 동작이후 꺼지지 않도록 한다.
+void main() {
+
+}
